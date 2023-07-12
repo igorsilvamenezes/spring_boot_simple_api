88d2364e3b6c039a6b659b1971c6ed992e87fe7d
Igor Menezes <igorsilvamenezes@gmail.com>
2023-07-12 19:02:13 -0300

    Initial commit

---
+++ src/main/java/com/menezes/SpringBootSimpleApiApplication.java
@@ -0,0 +1,13 @@
+package com.menezes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleApiApplication.class, args);
+	}
+
+}
